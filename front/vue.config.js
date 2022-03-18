da3bfd50eb956597ea9a8e219525811c68d3c64e
andrew_wang <andrew_wang@friday.tw>
2022-03-18 16:33:29 +0800

    更改 html title

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,5 +1,12 @@
 const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/istaging-interview/'
+  publicPath: '/istaging-interview/',
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.ts',
+      title: ' Tree 圖書館 🏛',
+    },
+  }
 })
